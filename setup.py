592971fef38dcf65a992d82e0d8b607faa0ac296
RPCastro <rafael.castro07@outlook.com>
2021-11-08 22:50:28 -0300

    Release 1.1.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+#long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
  
     name='pybacen',  # Required
-    version='1.0.7',  # Required
+    version='1.1.0',  # Required
     description='Economic analysis in the Brazilian scenario',  # Optional
-    long_description=long_description,
+    #long_description=long_description,
     author='Rafael Pereira de Castro',  # Optional
     author_email='rafael.castro07@outlook.com',  # Optional
 
